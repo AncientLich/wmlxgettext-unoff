0362a5313aad3a99436670e3271225674d179d70
Nobun <darknobunaga@yahoo.it>
2016-02-29 15:55:59 +0100

    removing useless example.d from docs

--- docs/source/srcdoc/example.d
+++
@@ -1,8 +0,0 @@
-digraph example {
-         a [label="sphinx", href="http://sphinx-doc.org", target="_top",
-            shape="diamond"];
-         b [label="other", shape="circle"];
-         SPACE [shape="record", label="{{SPACE|27}|00}"];
-         a -> b -> SPACE;
-         SPACE -> a;
-}
