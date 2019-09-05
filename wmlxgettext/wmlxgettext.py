e8d2fa3e48f5e90cb0182f1f04553a6d9a7bfedb
Nobun <darknobunaga@yahoo.it>
2019-09-05 09:37:09 +0200

    fix wesnoth bug #3469 + #4275

--- wmlxgettext/wmlxgettext.py
+++ wmlxgettext/wmlxgettext.py
@@ -5,18 +5,18 @@
 #
 # wmlxgettext -- generate a blank .pot file for official campaigns translations
 #                    (build tool for wesnoth core)
-#                
+#
 #
 # By Nobun, october 2015
 # Thanks to Elvish Hunter for writing code for coloring text under windows
 #
 #                              PURPOSE
 #
-# wmlxgettext is a python3 tool that replace the old (but very good) 
+# wmlxgettext is a python3 tool that replace the old (but very good)
 # perl script with the same name.
 # Replacing perl with python3 will ensure more portability.
 #
-# wmlxgettext is a tool that is directly used during wesnoth build process 
+# wmlxgettext is a tool that is directly used during wesnoth build process
 # to generate the pot files for the core campaigns.
 #
 #                              USAGE
@@ -27,7 +27,7 @@
 #
 #                   SOURCE CODE DOCUMENTATION
 #
-# While the source code contains some comments that explain what it does at 
+# While the source code contains some comments that explain what it does at
 # that point, the source code is mainly explained on source documentation at:
 # http://wmlxgettext-unoff.readthedocs.org/en/latest/srcdoc/index.html
 
@@ -54,7 +54,7 @@ def commandline(args):
     parser.add_argument(
         '--version',
         action='version',
-        version='wmlxgettext 2018.03.09.py3'
+        version='wmlxgettext 2019.09.03.py3'
     )
     parser.add_argument(
         '-o',
@@ -69,7 +69,7 @@ def commandline(args):
                '[**REQUIRED ARGUMENT**]')
     )
     parser.add_argument(
-        '--domain', 
+        '--domain',
         default='wmlxgettext',
         required=True,
         dest='domain',
@@ -78,7 +78,7 @@ def commandline(args):
                '[**REQUIRED ARGUMENT**]')
     )
     parser.add_argument(
-        '--directory', 
+        '--directory',
         default='.',
         dest='start_path',
         help=('Complete path of your "start directory". '
@@ -86,11 +86,11 @@ def commandline(args):
               'every WML/lua file should start from this directory.')
     )
     parser.add_argument(
-        '--initialdomain', 
+        '--initialdomain',
         default='wesnoth',
         dest='initdom',
         help=('Initial domain value on WML/lua file when no textdomain '
-              'setted in that WML/lua file.\nBy default it is equal to '
+              'set in that WML/lua file.\nBy default it is equal to '
               '"wesnoth" and usually you don\'t need to change this value.')
     )
     parser.add_argument(
@@ -122,29 +122,29 @@ def commandline(args):
         default=False,
         dest='fuzzy',
         help=("If you specify this flag, all sentences contained on the POT "
-              "file created by wmlxgettext will be setted as fuzzy.\n"
-              "By default sentences are NOT setted as fuzzy.")
+              "file created by wmlxgettext will be set as fuzzy.\n"
+              "By default sentences are NOT set as fuzzy.")
     )
     parser.add_argument(
         '--recursive',
         action='store_true',
         default=False,
         help=("If this option is used, wmlxgettext will scan recursively the "
-              "directory setted on the '--directory' parameter and checks "
+              "directory set on the '--directory' parameter and checks "
               "itself every WML/lua file. "
               "If this option is used, EXPLICIT LIST of files will be "
-              "ignored.") 
+              "ignored.")
     )
     parser.add_argument(
         'filelist',
         help='List of WML/lua files of your UMC (source files).',
         nargs='*'
     )
-    
+
     '''
     Developer Options - not suitable for standard usage:
     --DMode is a reserved flag used to verify how wmlxgettext is internally
-            working. When this flag is used (setted to ON), an extra
+            working. When this flag is used (set to ON), an extra
             file (debug.txt) will be created. debug.txt will contain
             useful informations to check if wmlxgettext is working as expected
             (but make sense only for wmlxgettext developers/contributors)
@@ -174,15 +174,14 @@ def main():
         args.outfile = None
     if args.debugmode:
         fdebug = open('debug.txt', 'w', encoding='utf-8')
-    pywmlx.statemachine.setup(sentlist, args.initdom, args.domain, 
+    pywmlx.statemachine.setup(sentlist, args.initdom, args.domain,
                               args.warnall, fdebug)
-    filelist = None
     if args.recursive is False and args.filelist is None:
         pywmlx.wmlerr("bad command line", "FILELIST must not be empty. "
                "Please, run wmlxgettext again and, this time, add some file "
                "in FILELIST or use the --recursive option.")
     elif args.recursive is False and len(args.filelist) <= 0:
-        pywmlx.wmlerr("bad command line", "FILELIST must not be empty. " 
+        pywmlx.wmlerr("bad command line", "FILELIST must not be empty. "
                "Please, run wmlxgettext again and, this time, add some file "
                "in FILELIST or use the --recursive option.")
     elif args.recursive is False:
@@ -195,18 +194,18 @@ def main():
                 "FILELIST will be ignored.")
         # If we use the --recursive option we recursively scan the add-on
         # directory.
-        #    But we want that the file reference informations placed 
+        #    But we want that the file reference informations placed
         # in the .po file will remember the (relative) root name of the
-        # addon. 
-        #    This is why the autof.autoscan function returns a tuple of 
-        # values: 
+        # addon.
+        #    This is why the autof.autoscan function returns a tuple of
+        # values:
         #   the first one is the parent directory of the original startPath
         #   the second one is the filelist (with the "fixed" file references)
         # This way, we can override startPath with its parent directory
-        # containing the main directory of the wesnoth add-on, without 
+        # containing the main directory of the wesnoth add-on, without
         # introducing bugs.
         startPath, filelist = pywmlx.autof.autoscan(startPath)
-    # this last case is equal to: 
+    # this last case is equal to:
     # if args.recursive is True and args.filelist is None:
     else:
         startPath, filelist = pywmlx.autof.autoscan(startPath)
@@ -215,16 +214,16 @@ def main():
         is_file = os.path.isfile(fname)
         if is_file:
             infile = None
-            try: 
+            try:
                 infile = open(fname, 'r', encoding="utf-8")
             except OSError as e:
                 errmsg = 'cannot read file: ' + e.args[1]
                 pywmlx.wmlerr(e.filename, errmsg, OSError)
             if fname.lower().endswith('.cfg'):
-                pywmlx.statemachine.run(filebuf=infile, fileref=fx, 
+                pywmlx.statemachine.run(filebuf=infile, fileref=fx,
                             fileno=fileno, startstate='wml_idle', waitwml=True)
             if fname.lower().endswith('.lua'):
-                pywmlx.statemachine.run(filebuf=infile, fileref=fx, 
+                pywmlx.statemachine.run(filebuf=infile, fileref=fx,
                             fileno=fileno, startstate='lua_idle', waitwml=False)
             infile.close()
     outfile = None
@@ -240,7 +239,7 @@ def main():
     pkgversion = args.package_version + '\\n"'
     print('msgid ""\nmsgstr ""', file=outfile)
     print('"Project-Id-Version:', pkgversion, file=outfile)
-    print('"Report-Msgid-Bugs-To: http://bugs.wesnoth.org/\\n"', file=outfile)
+    print('"Report-Msgid-Bugs-To: https://bugs.wesnoth.org/\\n"', file=outfile)
     now = datetime.utcnow()
     cdate = '{:04d}-{:02d}-{:02d} {:02d}:{:02d} UTC\\n"'.format(now.year,
                                                                 now.month,
